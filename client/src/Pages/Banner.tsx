a2c14b3eca1160132392d60790a84edc916a1d05
Mikaela Tasi <mikaela.tasi@fshnstudent.info>
2024-02-12 09:53:50 +0100

    Made some edits related to the topics

--- client/src/Pages/Banner.tsx
+++ client/src/Pages/Banner.tsx
@@ -10,17 +10,17 @@ function Banner() {
         >
           <div className="absolute left-0 bottom-0 w-full h-full z-10"
             style={{ backgroundImage: 'linear-gradient(180deg,transparent,rgba(0,0,0,.7))' }}></div>
-          <img src="https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80" className="absolute left-0 top-0 w-full h-full rounded z-0 object-cover" alt="First Example" />
+          <img src="https://worldofwanderlust.com/wp-content/uploads/2022/04/Paris-World-of-Wanderlust25-scaled.jpg" className="absolute left-0 top-0 w-full h-full rounded z-0 object-cover" alt="First Example" />
           <div className="p-4 absolute bottom-0 left-0 z-20">
-            <span className="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2">Nutrition</span>
+            <span className="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2">Travel</span>
             <h2 className="text-4xl font-semibold text-gray-100 leading-tight">
-              Pellentesque a consectetur velit, ac molestie ipsum. Donec sodales, massa et auctor.
+            These are the most visited cities in France.
             </h2>
             <div className="flex mt-3">
               <img src="https://randomuser.me/api/portraits/men/97.jpg" className="h-10 w-10 rounded-full mr-2 object-cover" alt="Author" />
               <div>
-                <p className="font-semibold text-gray-200 text-sm"> Mike Sullivan </p>
-                <p className="font-semibold text-gray-400 text-xs"> 14 Aug </p>
+                <p className="font-semibold text-gray-200 text-sm"> Stuart Roth </p>
+                <p className="font-semibold text-gray-400 text-xs"> 21 Aug </p>
               </div>
             </div>
           </div>
@@ -32,15 +32,15 @@ function Banner() {
         >
           <div className="absolute left-0 top-0 w-full h-full z-10"
             style={{ backgroundImage: 'linear-gradient(180deg,transparent,rgba(0,0,0,.7))' }}></div>
-          <img src="https://images.unsplash.com/photo-1543362906-acfc16c67564?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1301&q=80" className="absolute left-0 top-0 w-full h-full rounded z-0 object-cover" alt="Second Example" />
+          <img src="https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,w_730/at%2Fstyle%2F2023-09%2Fai-interior-design%2Fai-decorilla" className="absolute left-0 top-0 w-full h-full rounded z-0 object-cover" alt="Second Example" />
           <div className="p-4 absolute bottom-0 left-0 z-20">
-            <span className="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2">Science</span>
-            <h2 className="text-3xl font-semibold text-gray-100 leading-tight">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+            <span className="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2">DIY</span>
+            <h2 className="text-3xl font-semibold text-gray-100 leading-tight">Best Tips To Design A Space From Start To Finish</h2>
             <div className="flex mt-3">
               <img src="https://images-na.ssl-images-amazon.com/images/M/MV5BODFjZTkwMjItYzRhMS00OWYxLWI3YTUtNWIzOWQ4Yjg4NGZiXkEyXkFqcGdeQXVyMTQ0ODAxNzE@._V1_UX172_CR0,0,172,256_AL_.jpg" className="h-10 w-10 rounded-full mr-2 object-cover" alt="Author" />
               <div>
-                <p className="font-semibold text-gray-200 text-sm"> Chrishell Staus </p>
-                <p className="font-semibold text-gray-400 text-xs"> 15 Aug </p>
+                <p className="font-semibold text-gray-200 text-sm"> Zach Blair </p>
+                <p className="font-semibold text-gray-400 text-xs"> 15 June </p>
               </div>
             </div>
           </div>
--- client/src/components/Post/PostForm.tsx
+++ client/src/components/Post/PostForm.tsx
@@ -13,24 +13,24 @@ const PostForm = () => {
           >
             <div 
               className="h-48 lg:w-48 flex-none bg-cover text-center overflow-hidden opacity-75"
-              style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80")' }} 
-              title="deit is very important"
+              style={{ backgroundImage: 'url("https://media.pitchfork.com/photos/65c9771d44fd12d935916072/2:1/w_2240,c_limit/Usher,%20Alicia%20Keys.jpg")' }} 
+              title="Usher"
             ></div>
             <div className="bg-white rounded px-4 flex flex-col justify-between leading-normal">
               <div>
                 <div className="mt-3 md:mt-0 text-gray-700 font-bold text-2xl mb-2">
-                  Aliquam venenatis nisl id purus rhoncus, in efficitur sem hendrerit.
+                Watch Usher’s 2024 Super Bowl Halftime Show With Alicia Keys, Ludacris, More
                 </div>
                 <p className="text-gray-700 text-base">
-                  Duis euismod est quis lacus elementum, eu laoreet dolor consectetur. Pellentesque sed neque vel tellus lacinia elementum. Proin consequat ullamcorper eleifend.
+                Usher took the stage for the 2024 Super Bowl Halftime Show, and you can watch what went down below. The singer brought out Alicia Keys, Ludacris, Lil Jon, H.E.R., and Jermaine Dupri during his show, which he performed amid dozens of dancers and gymnasts in sparkly getups and feathered wings. 
                 </p>
               </div>
               <div className="flex mt-3">
                 <img src="https://randomuser.me/api/portraits/men/86.jpg"
                   className="h-10 w-10 rounded-full mr-2 object-cover" alt="Author" />
                 <div>
-                  <p className="font-semibold text-gray-700 text-sm capitalize"> eduard franz </p>
-                  <p className="text-gray-600 text-xs"> 14 Aug </p>
+                  <p className="font-semibold text-gray-700 text-sm capitalize"> Zachary Blackburn </p>
+                  <p className="text-gray-600 text-xs"> 14 November </p>
                 </div>
               </div>
             </div>
@@ -38,23 +38,23 @@ const PostForm = () => {
 
           <div className="rounded w-full lg:flex mb-10">
             <div className="h-48 lg:w-48 flex-none bg-cover text-center overflow-hidden opacity-75"
-              style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1515003197210-e0cd71810b5f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80")' }} title="deit is very important">
+              style={{ backgroundImage: 'url("https://media.pitchfork.com/photos/65c94e89c54c5b1b4a4185be/2:1/w_2240,c_limit/Beyonce%20.jpg")' }} title="Beyonce">
             </div>
             <div className="bg-white rounded px-4 flex flex-col justify-between leading-normal">
               <div>
                 <div className="mt-3 md:mt-0 text-gray-700 font-bold text-2xl mb-2">
-                  Integer commodo, sapien ut vulputate viverra
+                Beyoncé Announces New Album Act II, Shares Two New Songs: Listen
                 </div>
                 <p className="text-gray-700 text-base">
-                  Nam malesuada aliquet metus, ac commodo augue mollis sit amet. Nam bibendum risus sit amet metus semper consectetur. Proin consequat ullamcorper eleifend. Nam bibendum risus sit amet metus semper consectetur.
+                Beyoncé has announced a new album; Act II arrives March 29. She’s also graced us with two new songs: “Texas Hold ’Em” and “16 Carriages.” Listen to those below, and scroll down for Bey’s teaser clip.
                 </p>
               </div>
               <div className="flex mt-3">
                 <img src="https://randomuser.me/api/portraits/women/54.jpg"
                   className="h-10 w-10 rounded-full mr-2 object-cover" alt="Author" />
                 <div>
-                  <p className="font-semibold text-gray-700 text-sm capitalize"> Serenity Hughes </p>
-                  <p className="text-gray-600 text-xs"> 14 Aug </p>
+                  <p className="font-semibold text-gray-700 text-sm capitalize"> Marilyn Gates </p>
+                  <p className="text-gray-600 text-xs"> 4 March </p>
                 </div>
               </div>
             </div>
@@ -62,23 +62,23 @@ const PostForm = () => {
 
           <div className="rounded w-full lg:flex mb-10">
             <div className="h-48 lg:w-48 flex-none bg-cover text-center overflow-hidden opacity-75"
-              style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80")' }} title="deit is very important">
+              style={{ backgroundImage: 'url("https://thediyplaybook.com/wp-content/uploads/2024/01/organize-purge-toys-20.jpg")' }} title="interior">
             </div>
             <div className="bg-white rounded px-4 flex flex-col justify-between leading-normal">
               <div>
                 <div className="mt-3 md:mt-0 text-gray-700 font-bold text-2xl mb-2">
-                  Suspendisse varius justo eu risus laoreet fermentum non aliquam dolor
+                How to Purge, Declutter, and Organize Kids’ Toys
                 </div>
                 <p className="text-gray-700 text-base">
-                  Mauris porttitor, velit at tempus vulputate, odio turpis facilisis dui, vitae eleifend odio ipsum at odio. Phasellus luctus scelerisque felis eget suscipit.
+                We had a lovely Christmas together and our kids received some amazing gifts from friends and family. However, once the tree was taken down and the new items were stowed away, we realized our house was packed to the brim with toys. Every single cabinet and closet seemed to be bursting with stuff!
                 </p>
               </div>
               <div className="flex mt-3">
                 <img src="https://unavatar.io/github/1stevengrant"
                   className="h-10 w-10 rounded-full mr-2 object-cover" alt="Author" />
                 <div>
-                  <p className="font-semibold text-gray-700 text-sm capitalize"> Enrique Leonzo </p>
-                  <p className="text-gray-600 text-xs"> 14 Aug </p>
+                  <p className="font-semibold text-gray-700 text-sm capitalize"> Barnaby Ray </p>
+                  <p className="text-gray-600 text-xs"> 28 July </p>
                 </div>
               </div>
             </div>
