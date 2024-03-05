a3e7d6fb0da3785cd408fe97efc095da12b837d4
Mikaela Tasi <mikaela.tasi@fshnstudent.info>
2024-03-05 10:37:00 +0100

    added a .env file

--- server/src/db.js
+++ server/src/db.js
@@ -1,12 +1,14 @@
 // create the connection with the database
 import mysql from "mysql";
+import dotenv from "dotenv";
 
+dotenv.config('server\src\.env');
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Mikaela2003*",
-    database: "blog"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE
 })
 
 db.connect((error) => {
